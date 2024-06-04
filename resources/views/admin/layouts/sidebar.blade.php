e522786d9ee52a6553ababb92625223942869cb0
adityaeks <mohadityaekosaputra11@gmail.com>
2024-06-04 17:03:47 +0700

    Hapus brand FE di admin

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,11 +1,5 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
-        <div class="sidebar-brand">
-            <a href="">{{ $settings->site_name }}</a>
-        </div>
-        <div class="sidebar-brand sidebar-brand-sm">
-            <a href="">||</a>
-        </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
             <li class="dropdown active">
@@ -32,7 +26,6 @@
 
             <li
                 class="dropdown {{ setActive([
-                    'admin.brand.*',
                     'admin.products.*',
                     'admin.products-image-gallery.*',
                     'admin.products-variant.*',
@@ -43,8 +36,6 @@
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-box"></i>
                     <span>Manage Products</span></a>
                 <ul class="dropdown-menu">
-                    <li class="{{ setActive(['admin.brand.*']) }}"><a class="nav-link"
-                            href="{{ route('admin.brand.index') }}">Brands</a></li>
                     <li
                         class="{{ setActive([
                             'admin.products.*',
