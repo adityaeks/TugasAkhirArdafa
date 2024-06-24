cede376401693fed0fde034b117bd1571bd6433d
adityaeks <mohadityaekosaputra11@gmail.com>
2024-06-24 19:02:39 +0700

    Update controller and datatables

--- app/Http/Controllers/Frontend/FrontendProductController.php
+++ app/Http/Controllers/Frontend/FrontendProductController.php
@@ -16,108 +16,48 @@ use Illuminate\Support\Facades\Session;
 class FrontendProductController extends Controller
 {
     public function productsIndex(Request $request)
-    {
-        if($request->has('category')){
-            $category = Category::where('slug', $request->category)->firstOrFail();
-            $products = Product::withAvg('reviews', 'rating')->withCount('reviews')
-            ->with(['variants', 'category', 'productImageGalleries'])
-            ->where([
-                'category_id' => $category->id,
-                'status' => 1,
-                'is_approved' => 1
-            ])
-            ->when($request->has('range'), function($query) use ($request){
-                $price = explode(';', $request->range);
-                $from = $price[0];
-                $to = $price[1];
+{
+    if($request->has('category')){
+        $category = Category::where('slug', $request->category)->firstOrFail();
+        $products = Product::with(['category', 'productImageGalleries'])
+        ->where([
+            'category_id' => $category->id,
+            'status' => 1,
+            'is_approved' => 1
+        ])
+        ->when($request->has('range'), function($query) use ($request){
+            $price = explode(';', $request->range);
+            $from = $price[0];
+            $to = $price[1];
 
-                return $query->where('price', '>=', $from)->where('price', '<=', $to);
-            })
-            ->paginate(12);
-        }elseif($request->has('subcategory')){
-            $category = SubCategory::where('slug', $request->subcategory)->firstOrFail();
-            $products = Product::withAvg('reviews', 'rating')->withCount('reviews')
-            ->with(['variants', 'category', 'productImageGalleries'])
-            ->where([
-                'sub_category_id' => $category->id,
-                'status' => 1,
-                'is_approved' => 1
-            ])
-            ->when($request->has('range'), function($query) use ($request){
-                $price = explode(';', $request->range);
-                $from = $price[0];
-                $to = $price[1];
-
-                return $query->where('price', '>=', $from)->where('price', '<=', $to);
-            })
-            ->paginate(12);
-        }elseif($request->has('childcategory')){
-            $category = ChildCategory::where('slug', $request->childcategory)->firstOrFail();
-
-            $products = Product::withAvg('reviews', 'rating')->withCount('reviews')
-            ->with(['variants', 'category', 'productImageGalleries'])
-            ->where([
-                'child_category_id' => $category->id,
-                'status' => 1,
-                'is_approved' => 1
-            ])
-            ->when($request->has('range'), function($query) use ($request){
-                $price = explode(';', $request->range);
-                $from = $price[0];
-                $to = $price[1];
-
-                return $query->where('price', '>=', $from)->where('price', '<=', $to);
-            })
-            ->paginate(12);
-        }elseif($request->has('brand')){
-           $brand = Brand::where('slug', $request->brand)->firstOrFail();
-
-            $products = Product::withAvg('reviews', 'rating')->withCount('reviews')
-            ->with(['variants', 'category', 'productImageGalleries'])
-            ->where([
-                'brand_id' => $brand->id,
-                'status' => 1,
-                'is_approved' => 1
-            ])
-            ->when($request->has('range'), function($query) use ($request){
-                $price = explode(';', $request->range);
-                $from = $price[0];
-                $to = $price[1];
-
-                return $query->where('price', '>=', $from)->where('price', '<=', $to);
-            })
-            ->paginate(12);
-        }elseif($request->has('search')){
-            $products = Product::withAvg('reviews', 'rating')->withCount('reviews')
-            ->with(['variants', 'category', 'productImageGalleries'])
-            ->where(['status' => 1, 'is_approved' => 1])
-            ->where(function ($query) use ($request){
-                $query->where('name', 'like', '%'.$request->search.'%')
-                    ->orWhere('long_description', 'like', '%'.$request->search.'%')
-                    ->orWhereHas('category', function($query) use ($request){
-                        $query->where('name', 'like', '%'.$request->search.'%')
-                            ->orWhere('long_description', 'like', '%'.$request->search.'%');
-                    });
-            })
-            ->paginate(12);
-
-        }else {
-            $products = Product::withAvg('reviews', 'rating')->withCount('reviews')
-            ->with(['variants', 'category', 'productImageGalleries'])
-            ->where(['status' => 1, 'is_approved' => 1])->orderBy('id', 'DESC')->paginate(12);
-        }
-
-        $categories = Category::where(['status' => 1])->get();
-        // banner ad
-        $productpage_banner_section = Adverisement::where('key', 'productpage_banner_section')->first();
-        $productpage_banner_section = json_decode($productpage_banner_section?->value);
-
-        return view('frontend.pages.product', compact('products', 'categories', 'productpage_banner_section'));
+            return $query->where('price', '>=', $from)->where('price', '<=', $to);
+        })
+        ->paginate(12);
+    } elseif($request->has('search')){
+        $products = Product::with(['category', 'productImageGalleries'])
+        ->where(['status' => 1, 'is_approved' => 1])
+        ->where(function ($query) use ($request){
+            $query->where('name', 'like', '%'.$request->search.'%')
+                ->orWhere('long_description', 'like', '%'.$request->search.'%')
+                ->orWhereHas('category', function($query) use ($request){
+                    $query->where('name', 'like', '%'.$request->search.'%')
+                        ->orWhere('long_description', 'like', '%'.$request->search.'%');
+                });
+        })
+        ->paginate(12);
     }
-    /** Show product detail page */
+
+    $categories = Category::where(['status' => 1])->get();
+    // atur benner
+    $productpage_banner_section = Adverisement::where('key', 'productpage_banner_section')->first();
+    $productpage_banner_section = json_decode($productpage_banner_section?->value);
+
+    return view('frontend.pages.product', compact('products', 'categories', 'productpage_banner_section'));
+}
+
     public function showProduct(string $slug)
     {
-        $product = Product::with(['vendor', 'category', 'productImageGalleries', 'variants', 'brand'])->where('slug', $slug)->where('status', 1)->first();
+        $product = Product::with(['vendor', 'category', 'productImageGalleries'])->where('slug', $slug)->where('status', 1)->first();
         $reviews = ProductReview::where('product_id', $product->id)->where('status', 1)->paginate(10);
         return view('frontend.pages.product-detail', compact('product', 'reviews'));
     }
